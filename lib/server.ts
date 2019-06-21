65243c0c61ddf9b47d25d92398aba67fffe57391
Jake Streb <jake@getgrist.com>
2019-06-21 12:40:12 -0400

    Added log

--- lib/server.ts
+++ lib/server.ts
@@ -89,7 +89,7 @@ function startComms(swiper: Swiper): void {
 
   // Message from telegram.
   app.post("/telegram", (req, res) => {
-    console.warn('GOT MSG!');
+    console.warn('GOT MSG!', req.body);
     acceptMsgFromClient('telegram', req.body.id, req.body.message)
     .catch(err => {
       logError(`Error handling telegram request "${req.body.message}": ${err}`);
