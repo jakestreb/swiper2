1194374cd89aa3d0a445e371fa40e656444e635e
Jake Streb <jake@getgrist.com>
2022-05-30 01:16:27 -0400

    log on received message

--- lib/io/CommManager.ts
+++ lib/io/CommManager.ts
@@ -81,6 +81,7 @@ export default class CommManager {
 
   private addTelegramListener() {
     this.telegramBot.on("text", (message: any) => {
+      log.debug(`Received telegram message: ${message.text}`);
       this.handleClientMsg('telegram', message.chat.id, message.text)
       .catch(err => {
         log.error(`Error handling telegram request "${message}": ${err}`);
