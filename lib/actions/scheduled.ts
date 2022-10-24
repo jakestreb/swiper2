49727826f99d5184cc7e218d1e611f18c0c89351
Jake Streb <jake@getgrist.com>
2022-10-24 01:53:03 -0400

    best formatting

--- lib/actions/scheduled.ts
+++ lib/actions/scheduled.ts
@@ -31,8 +31,8 @@ export async function scheduled(this: Swiper, convo: Conversation): Promise<Swip
   let rows: string[] = [];
   if (shows.length > 0 && movies.length > 0) {
     rows = [
-      [f.i('M O V I E S'), ...movies].join('\n'),
-      [f.i('T V'), ...shows].join('\n'),
+      [`${f.sp(2)}${f.u('M O V I E S')}`, ...movies].join('\n'),
+      [`${f.sp(2)}${f.u('S H O W S')}`, ...shows].join('\n'),
     ];
   } else {
     rows = [...movies, ...shows];
